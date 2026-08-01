15b91871599d04193ab8dadc7c11c14756706540
Sanjanagowda31 <sanjanashekar3110@gmail.com>
2026-08-01 19:54:16 +0530

    Implement String palindrome using .equals()

---
+++ palindrome/src/palindrome/palindrome.java
@@ -0,0 +1,21 @@
+package palindrome;
+import java.util.Scanner;
+public class palindrome 
+{
+  public static void main(String args[])
+  {
+	  Scanner sc = new Scanner(System.in);
+	  System.out.print("Enter the string:");
+	  String str=sc.nextLine();
+	  
+	  StringBuffer sb = new StringBuffer(str);
+      String reversed = sb.reverse().toString();
+      
+      if(str.equals(reversed))
+      {
+    	  System.out.println("This string is palindrome");
+      }
+      else
+    	  System.out.println(" this string is not palindrome");
+  }
+}
