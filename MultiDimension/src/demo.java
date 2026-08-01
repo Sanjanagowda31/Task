06449dddd5bda0571e90e0f578e98b6b8e6d083b
Sanjanagowda31 <sanjanashekar3110@gmail.com>
2026-08-01 17:43:38 +0530

    printing 2D array by transposing columns and rows

---
+++ MultiDimension/src/demo.java
@@ -0,0 +1,21 @@
+
+public class demo {
+
+	public static void main(String[] args)
+	{
+		int[][] arr={
+			         {2,3,4},
+				     {1,2,3},
+				     {3,4,5}
+	                };
+		for(int i=0;i<3;i++)
+		{
+			for(int j=0;j<3;j++)
+			{
+				System.out.print(arr[j][i] + " ");
+			}
+			System.out.println();
+		}
+	}	
+
+}
