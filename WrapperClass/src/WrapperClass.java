2bb66f4520d00a4540443be7218d1be21d7ebd52
Sanjanagowda31 <sanjanashekar3110@gmail.com>
2026-08-05 01:50:54 +0530

    Demonstrate wrapper class example for autoboxing and unboxing

---
+++ WrapperClass/src/WrapperClass.java
@@ -0,0 +1,17 @@
+
+public class WrapperClass 
+{
+   public static void main(String args[]) 
+   {
+	   int n=7;
+	   Integer num1=n;
+	   
+	   int num2=num1;
+	   System.out.println(num2);
+	   
+	   String s="100";
+	   Integer s1=Integer.parseInt(s);
+	   System.out.println(s1);
+	   
+   }
+}
