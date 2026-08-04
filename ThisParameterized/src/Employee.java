9331abbbe5cd9f4cf3a36c92b936c28371e4bfbf
Sanjanagowda31 <sanjanashekar3110@gmail.com>
2026-08-05 01:32:34 +0530

    Demonstrate 'this' reference in parameterized constructor

---
+++ ThisParameterized/src/Employee.java
@@ -0,0 +1,34 @@
+
+public class Employee 
+{
+    private int id;
+    private String name;
+    
+    public int getId()
+    {
+    	return id;
+    }
+    public void setId(int id)
+    {
+    	this.id=id;
+    }
+    
+    public String getName()
+    {
+    	return name;
+    }
+    public void setName(String name)
+    {
+    	this.name=name;
+    }
+}
+class Main 
+{
+   public static void main (String args[])
+   {
+	   Employee obj=new Employee();
+	   obj.setId(101);
+	   obj.setName("sanjana");
+	   System.out.println("name : "  + obj.getName() + " , " + "ID : " + obj.getId());
+   }
+}
