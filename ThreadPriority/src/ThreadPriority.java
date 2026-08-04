e8294f20983bf1c34299e73ac66d59f169f38ee4
Sanjanagowda31 <sanjanashekar3110@gmail.com>
2026-08-05 01:34:30 +0530

    showcase thread scheduling via priorities and sleep intervals

---
+++ ThreadPriority/src/ThreadPriority.java
@@ -0,0 +1,55 @@
+class A extends Thread
+{
+	public void run()
+	{
+		for(int i=1;i<=100;i++)
+		{
+			System.out.println("Hi");
+			try
+			{
+				Thread.sleep(10);
+			}
+			catch(InterruptedException e)
+			{
+				e.printStackTrace();
+			}
+		}
+	}
+}
+class B extends Thread
+{
+	public void run()
+	{
+		for(int i=1;i<=100;i++)
+		{
+			System.out.println("Hello");
+			try
+			{
+				Thread.sleep(10);
+			}
+			catch(InterruptedException e)
+			{
+				e.printStackTrace();
+			}
+		}
+	}
+}
+public class ThreadPriority 
+{
+   public static void main(String args[])
+   {
+	   A obj = new A ();
+	   B obj1 = new B();
+	   
+	   obj.start();
+	   try
+		{
+			Thread.sleep(10);
+		}
+		catch(InterruptedException e)
+		{
+			e.printStackTrace();
+		}
+	   obj1.start();
+   }
+}
